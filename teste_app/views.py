18b8470065dfacbd256762257b570b16ebb72cfb
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-08-21 19:49:27 -0300

    teste_app app created and configurated

--- teste_app/views.py
+++ teste_app/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.generic.edit import FormView
+from django.shortcuts import redirect
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import GenerateRandomUserForm
+from .tasks import create_random_user_accounts
+
+
+def teste_view(request):
+    form = GenerateRandomUserForm(request.POST)
+
+   
+    
+    context = {
+        'form':form,
+    }
+
+    return render(request, 'teste.html', context)
+
+        
